01eeae2ea6216770b6918a8a15d1039b4a0a145f
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-03 16:50:02 +0200

    struttura header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script>
+    import HeaderComp from './components/HeaderComp.vue';
+
+    export default {
+      name: 'App',
+      components: {
+        HeaderComp,
+      }
+    }
 </script>
 
 <template>
+  <div id="title">
+    <HeaderComp/>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
+// @use './style/partials/_variables.scss' as *;
+@use "./style/main.scss" as *;
+
 </style>
+
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,71 @@
+<script>
+    export default{
+        name: "HeaderComp",
+        data() {
+            return {
+                navbar: [
+                    {
+                        menuName: "characters",
+                        url: "/characters"
+                    },
+                    {
+                        menuName: "comics",
+                        url: "/comics"
+                    },
+                    {
+                        menuName: "movies",
+                        url: "/movies"
+                    },
+                    {
+                        menuName: "tv",
+                        url: "/tv"
+                    },
+                    {
+                        menuName: "games",
+                        url: "/games"
+                    },
+                    {
+                        menuName: "collectibles",
+                        url: "/collectibles"
+                    },
+                    {
+                        menuName: "videos",
+                        url: "/videos"
+                    },
+                    {
+                        menuName: "fans",
+                        url: "/fans"
+                    },
+                    {
+                        menuName: "news",
+                        url: "/news"
+                    },
+                    {
+                        menuName: "shop",
+                        url: "/characters"
+                    },
+                ]            
+            }
+        }
+    }
+    </script>
+
+    <template>
+        <header>
+            <div id="logo">
+                <img src="/images/dc-logo.png" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li v-for="(element,index) in navbar">
+                        <a :href="element.url">
+                            {{ element.menuName }}
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </template>
+
+    <style>
+    </style>
